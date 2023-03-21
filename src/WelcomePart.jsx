e95c4982203dcd0bb82485fbb3088be6f3b39f5a
elenavetisyan333 <elen.avetisyan.333@mail.ru>
2023-03-21 23:24:02 +0400

    add language context to welcome part

--- src/WelcomePart.jsx
+++ src/WelcomePart.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import styles from './WelcomePart.module.css'
+import { LangContext } from './App';
 
 function WelcomePart() {
+  const {language} = useContext(LangContext);
+
   return (
     <div className={styles.welcomePart}>
         <div className={styles.info}>
             <h1>Ararat</h1>
             <h3>IT School</h3>
-            <p>Բացահայտիր IT անսահման հնարավորությունները</p>
-            <button>Դասընթացներ</button>
+            <p>{
+              language == "arm" ? "Բացահայտիր IT անսահման հնարավորությունները" : language == "ru" ? "Откройте для себя безграничные возможности IT" : "Discover the endless possibilities of IT"
+              }</p>
+            <button>{
+              language == "arm" ? "Դասընթացներ" : language == "ru" ? "Курсы" : "Courses"
+              }</button>
         </div>
         <img src="../Images/main_image.png" alt="" width={553} height={553}/>
     </div>
