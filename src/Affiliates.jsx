c9e770457e1a652e1dd7a4ededc79e74dc434655
elenavetisyan333 <elen.avetisyan.333@mail.ru>
2023-03-21 23:37:54 +0400

    add language context to Affiliates

--- src/Affiliates.jsx
+++ src/Affiliates.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import styles from './Affiliates.module.css'
 import Affiliate from './Affiliate'
+import { LangContext } from './App'
+
 
 function Affiliates() {
+  const {language} = useContext(LangContext);
+
   return (
     <div className={styles.affiliatesPart}>
-        <h2>Մասնաճյուղեր</h2>
+        <h2>{
+          language == "arm" ? "Մասնաճյուղեր" : language == "ru" ? "Филиалы" : "Affiliates"
+          }</h2>
         <div className={styles.affiliates}>
-            <Affiliate image="../Images/office_ararat.jpg" city="Ք. Արարատ" />
-            <Affiliate image="../Images/office_yerevan.jpeg" city="Ք. Երևան" />
+            <Affiliate image="../Images/office_ararat.jpg" city={language == "arm" ? "Ք. Արարատ" : language == "ru" ? "Г․ Арарат" : "C. Ararat"} />
+            <Affiliate image="../Images/office_yerevan.jpeg" city={language == "arm" ? "Ք. Երևան" : language == "ru" ? "Г․ Ереван" : "C. Yerevan"} />
         </div>
     </div>
   )
