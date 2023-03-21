3e5d285c06c37dcb21a70e83206b15261df8bc66
elenavetisyan333 <elen.avetisyan.333@mail.ru>
2023-03-21 23:55:06 +0400

    finish the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React , { useState } from 'react'
 import './App.css'
 import Header from './Header'
 import WelcomePart from './WelcomePart'
@@ -7,15 +7,20 @@ import Affiliates from './Affiliates'
 import Opportunities from './Opportunities'
 import Footer from './Footer'
 
+export const LangContext = React.createContext();
 function App() {
+  const [language, setLanguage] = useState("arm");
+
   return (
     <>
-      <Header />
-      <WelcomePart />
-      <Courses />
-      <Affiliates />
-      <Opportunities />
-      <Footer />
+      <LangContext.Provider value={{language, setLanguage}}>
+        <Header />
+        <WelcomePart />
+        <Courses />
+        <Affiliates />
+        <Opportunities />
+        <Footer />
+     </LangContext.Provider>
     </>
   )
 }
